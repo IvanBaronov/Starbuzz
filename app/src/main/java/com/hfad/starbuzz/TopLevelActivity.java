32664fb1cc1d2caa1447159064ea826ab7f6639b
IvanBaronov <68464530+IvanBaronov@users.noreply.github.com>
2022-05-24 22:03:04 +0300

    Updated TopLevelActivity. Added an additional ListView with favorite drinks.

--- app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -2,20 +2,34 @@ package com.hfad.starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.content.Intent;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 
 public class TopLevelActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor favoritesCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        setupOptionsListView();
+        setupFavoritesListView();
+    }
 
+    private void setupOptionsListView() {
         AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
@@ -28,4 +42,43 @@ public class TopLevelActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list_options);
         listView.setOnItemClickListener(itemClickListener);
     }
+
+    private void setupFavoritesListView() {
+        ListView listFavorites = (ListView) findViewById(R.id.list_favorites);
+        try {
+            SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            favoritesCursor = db.query("DRINK",
+                    new String[] {"_id", "NAME"},
+                    "FAVORITE = 1",
+                    null, null, null, null);
+            CursorAdapter favoriteAdapter = new SimpleCursorAdapter(TopLevelActivity.this,
+                    android.R.layout.simple_list_item_1,
+                    favoritesCursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0);
+            listFavorites.setAdapter(favoriteAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Data unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        listFavorites.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View v, int position, long id) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int)id);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        favoritesCursor.close();
+        db.close();
+    }
+
 }
